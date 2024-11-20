f9747c8b2211bc8462a26b808ec523a4a27d543a
dankren <daniilruzenko@gmail.com>
2024-11-20 17:52:17 +0300

    1

--- Practic13/main13.c
+++ Practic13/main13.c
@@ -12,24 +12,95 @@ int main()
 	return 0;
 }
 
-#define N 10
 int Task1() {
-	setlocale(LC_CTYPE, "RUS");
-	printf("Введите свое имя: ");
-	char* gets(char* str);
-	char s[6];
-	scanf_s("%s", &s);
-	char new_s[] = strcat("Привет,",const char *s);
+
+	char masstr[100] = "";
+	fgets(masstr, sizeof(masstr),stdin);
+	char start[100] = " hi ";
+	rsize_t Size = sizeof(start) - strlen(start) - 1;
+	strncat_s(start, sizeof(start), masstr, Size);
+	start[Size + 1] = '/0';
+	for (int i = 0; i < strlen(start); i++)
+	{
+		printf("%c", toupper(start[i]));
+	}
+	//Добавьте к строке приветствия множество восклицательных знаков так, чтобы суммарная длина строки составляла 40 символов. 
+	rsize_t Size40 = 40 * sizeof(char);
+	strncat_s(start, sizeof(start), "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", Size40);
+	printf("%s", start);
 	return 0;
 }
-int Task2() {
-	setlocale(LC_CTYPE, "RUS");
 
+//Измените первоначальный вариант программы, выводящий строку типа «Привет, Вася!» так, чтобы 
+//4) перед каждой последней буквой вставлялся символ подчеркивания;
+int Task2() {
+	char masstr[100] = "";
+	char masstr2[100] = "";
+	fgets(masstr, sizeof(masstr), stdin);
+	char start[100] = "hi, ";
+	char start2[100] = "";
+	int index_mstr2 = strlen(masstr) - 1;
+	int index_start2 = strlen(start) - 1;
+	for (int i = 0; i < strlen(masstr); i++) {
+		if (i == index_mstr2 - 1)
+		{
+			masstr2[i] = '_';
+			i++;
+			masstr2[i] = masstr[i - 1];
+		}
+		else {
+			masstr2[i] = masstr[i];
+		}	
+	}
+	masstr2[strlen(masstr2) + 1] = ' ';
+
+	for (int i = 0; i < strlen(start); i++) {
+		if (i == index_start2 -1 )
+		{
+			start2[i] = '_';
+			i++;
+			start2[i] = start[i - 1];
+		}
+		else {
+			start2[i] = start[i];
+		}
+	}
+	start2[strlen(start2) + 1] = ' ';
+
+	rsize_t Size = sizeof(start2) - strlen(start2) - 1;
+	strncat_s(start2, sizeof(start2), masstr2, Size);
+	start2[Size + 1] = '/0';
+	printf("%s", start2);
 	return 0;
 }
 
 int Task3() {
-	setlocale(LC_CTYPE, "RUS");
+//самое длинное слово в строке 
+	char masstr[100] = "";
+	char clov[100] = "";
+	int ind_start = 0;
+	int sum_left = 0;
+	int sum_right = 0;
+	fgets(masstr, sizeof(masstr), stdin);
+	int ind_end = strlen(masstr) - 1;
+	for (int i = 0; i <= strlen(masstr) - 1; i++) {
+		if (masstr[i] != ' ')
+		{
+			sum_left++;
+		}
+		else {
+			if (sum_left > sum_right)
+			{
+				sum_right = sum_left;
+				sum_left = 0;
+				ind_end = i - 1;
+				ind_start = ind_end - sum_right;
+			}
+			else {
+				sum_left == 0;
+			}
+		}
+	}
 
 	return 0;
 }
